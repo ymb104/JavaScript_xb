5dd98ba7ccb92713b4ed63d66447a4af92b53a75
ymb104 <838196333@qq.com>
2022-11-13 17:44:33 +0800

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,17 @@
+//可以判断原始类型 除了null
+console.log(typeof 2); //number
+console.log(typeof true); //boolean
+console.log(typeof 'str'); //string
+console.log(typeof undefined);
+//undefined
+
+console.log(typeof null);
+//object
+
+console.log(typeof []);
+//object
+
+console.log(typeof function () { });
+//function
+console.log(typeof {});
+//object
